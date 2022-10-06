dc2196c44cbd9b20945bbdf9a65ae2640f407c00
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-06 12:04:23 +0100

    🚀 Implement the useFetch Hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
+import { useFetch } from "../hooks/useFetch"
+
+
+//Styles
+import "./Home.css"
+
 export default function Home(){
+    const { data: articles, isPending, error } = useFetch('http://localhost:3000/articles')
+
+
     return(
-        <div>
-            <h2>Home Page</h2>
-            <p>Lorizzle ipsizzle dolizzle sure amizzle, we gonna chung crunk elizzle. Hizzle fo shizzle velizzle, i'm in the shizzle volutpizzle, things boom shackalack, gravida vizzle, rizzle. Pellentesque crackalackin tortor. Sed erizzle. Rizzle izzle you son of a bizzle dapibizzle shizznit tempizzle tellivizzle. Maurizzle crackalackin nibh et check it out. You son of a bizzle cool you son of a bizzle. Pellentesque rizzle mammasay mammasa mamma oo sa nisi. In shiz crackalackin platea dictumst. Boofron dapibizzle. Things tellus urna, owned eu, mattizzle ac, sizzle vitae, nunc. Crazy suscipit. Integer i'm in the shizzle owned sizzle mofo.</p>
+        <div className="home">
+
+            <h2>Articles</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+
+            {articles && articles.map((article) => (
+                <div key={article.id} className="card">
+                    <h3>{article.title}</h3>
+                    <p>{article.author}</p>
+                </div> 
+            ))}
+            
         </div>
     )
 }
